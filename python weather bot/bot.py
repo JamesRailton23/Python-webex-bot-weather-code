90418c11d528854d5efc6ad5d3cd4e01bc6171b7
James Railton <jamesrailton2003@gmail.com>
2023-01-19 08:35:00 +0000

    Initial commit

---
+++ python weather bot/bot.py
@@ -0,0 +1,28 @@
+from weather import WeatherByZIP
+
+from webex_bot.commands.echo import EchoCommand
+from webex_bot.commands.help import HelpCommand
+
+from webex_bot.webex_bot import WebexBot
+
+
+bot = WebexBot(
+    teams_bot_token=(
+        "YWZjMjNhMWItZTgxMC00MTNhLTkxOGYtZmRkZTc0Y2Q0ZWEzZDI0OTRiOTctZDI4_PE93_83163ff1-9c48-440d-9531-7664a5bc3cc6"
+    ),
+    include_demo_commands=True,
+)
+
+
+bot.add_command(EchoCommand())
+bot.add_command(WeatherByZIP())
+bot.add_command(
+    HelpCommand(
+        bot_name="wexbot",
+        bot_help_subtitle="",
+        bot_help_image="https://i.postimg.cc/2jMv5kqt/AS89975.jpg",
+    )
+)
+
+# Call `run` for the bot to wait for incoming messages.
+bot.run()
---
+++ python weather bot/weather.py
@@ -0,0 +1,43 @@
+from webex_bot.models.command import Command
+import logging
+import requests
+import json
+
+log = logging.getLogger(__name__)
+
+
+class WeatherByZIP(Command):
+    def __init__(self):
+        super().__init__(
+            command_keyword="weather",
+            help_message="Get current weather conditions by post code.",
+            card=None,
+        )
+
+    def execute(self, message, attachment_actions, activity):
+
+        zip_code = message.strip()
+
+        # Define our URL, with requested ZIP code & API Key
+        APIkey = "8cad54f3ba7b5b2079952160429b173a"
+        url = "https://api.openweathermap.org/data/2.5/weather?"
+        url += f"zip={zip_code},GB&units=metric&appid={APIkey}"
+
+        # Query weather
+        response = requests.get(url)
+        weather = response.json()
+
+        # Pull out desired info
+
+        city = weather["name"]
+        conditions = weather["weather"][0]["description"]
+        temperature = weather["main"]["temp"]
+        humidity = weather["main"]["humidity"]
+        wind = weather["wind"]["speed"]
+
+        response_message = (
+            f"In {city}, it's currently {temperature}*c with {conditions}. "
+        )
+        response_message += f"Wind speed is {wind}mph. Humidity is {humidity}%"
+
+        return response_message
